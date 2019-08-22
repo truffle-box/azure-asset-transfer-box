3edd5050f5c1094c30fbe0a7064d041c0e6aa4ed
Cale Teeter <cale.teeter@microsoft.com>
2019-08-21 20:58:42 -0700

    Updated configuration

--- truffle-config.js
+++ truffle-config.js
@@ -6,13 +6,6 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
-    },
-    ctconsort: {
-      network_id: "*",
-      gas: 0,
-      gasPrice: 0,
-      provider: new HDWalletProvider(fs.readFileSync('c:\\Users\\caleteet\\Downloads\\coreabs.env', 'utf-8'), "https://ctmember1.blockchain.azure.com:3200/fBhCnIgrvWpzxg1sJqSSo3pG"),
-      consortium_id: 1566440437029
     }
   },
   mocha: {},
